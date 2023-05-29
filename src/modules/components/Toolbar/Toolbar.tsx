a19fc74ce2f4a85c997ccc5d7e1ced26764bee6c
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-05-29 23:58:13 +0300

    Implement simple toolbar

--- src/modules/components/Toolbar/Toolbar.tsx
+++ src/modules/components/Toolbar/Toolbar.tsx
@@ -1,7 +1,32 @@
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
-const Toolbar = () => {
-  return <Box>Toolbar</Box>;
+const Toolbar = ({ pathLower }: { pathLower: string }) => {
+  return (
+    <Box
+      sx={{
+        mt: "30px",
+        mb: "30px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "15px",
+      }}
+    >
+      <Typography variant="h5">{pathLower}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          gap: "10px",
+        }}
+      >
+        <Button variant="contained" size="small">
+          Copy to DropBox
+        </Button>
+        <Button variant="contained" size="small">
+          Download
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default Toolbar;
