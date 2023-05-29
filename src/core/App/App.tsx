7d29b44d474bef1f19982891ab4509bb0f7f70bf
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-05-29 22:43:37 +0300

    Refactor App component

--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -1,36 +1,21 @@
-import { useEffect } from "react";
-import { Dropbox } from "dropbox";
 import { CssBaseline, Divider } from "@mui/material";
 import Header from "../../modules/components/Header/Header";
+import Finder from "../../modules/components/Finder/Finder";
+import { Route, Routes } from "react-router-dom";
+
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
-  const accessToken = import.meta.env.VITE_TOKEN;
-  const sharedLink =
-    "https://www.dropbox.com/scl/fo/apbz61su4adn55q0h1ye0/h?dl=0&rlkey=h8oqwsgmu5i8ulr0lrpok51pm";
-
-  const dbx = new Dropbox({ accessToken });
-
-  useEffect(() => {
-    dbx
-      .sharingGetSharedLinkMetadata({ url: sharedLink })
-      .then((res) => {
-        const path = res.result.path_lower || "";
-        return dbx.filesListFolder({ path });
-      })
-      .then((res) => {
-        console.log(res.result);
-      })
-      .catch(function (error) {
-        console.error(error, "111111");
-      });
-  }, []);
-
   return (
     <>
       <CssBaseline />
-      <Header />
-      <Divider />
-      <p>Hello world</p>
+      <BrowserRouter>
+        <Header />
+        <Divider />
+        <Routes>
+          <Route path={"*"} element={<Finder />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
